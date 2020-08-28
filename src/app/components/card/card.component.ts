5000e45937a4c7f5664945339a18e4c7584e5edb
Eder Gabriel <eder1602@outlook.com>
2020-08-28 20:58:20 -0300

    Introdução de animação

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,7 +1,29 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { trigger, state, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-card',
+  animations: [
+    trigger('openClose', [
+      // ...
+      state('open', style({
+        height: '200px',
+        opacity: 1,
+        backgroundColor: 'yellow'
+      })),
+      state('closed', style({
+        height: '100px',
+        opacity: 0.5,
+        backgroundColor: 'green'
+      })),
+      transition('open => closed', [
+        animate('1s')
+      ]),
+      transition('closed => open', [
+        animate('0.5s')
+      ]),
+    ]),
+  ],
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss']
 })
@@ -9,8 +31,14 @@ export class CardComponent implements OnInit {
 
   @Input() teste: any;
   @Output() numero = new EventEmitter();
+  isOpen = true;
 
   constructor() { }
+
+
+  toggle() {
+    this.isOpen = !this.isOpen;
+  }
 
   ngOnInit() {
     this.numero.emit(2);
--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ValidationFormHelper } from '../../components/helper/validation-form.helper';
+import { AbstractControl, FormGroup, FormBuilder, Validators, FormArray, FormControl, ValidatorFn } from '@angular/forms';
 
 @Component({
   selector: 'app-config',
   templateUrl: './config.component.html',
   styleUrls: ['./config.component.scss']
 })
-export class ConfigComponent implements OnInit {
+export class ConfigComponent extends ValidationFormHelper implements OnInit {
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder,
+              private router: Router) { 
+    super();
+  }
 
   ngOnInit() {
+    this.form = new FormGroup({
+      username: this.formBuilder.control('', [Validators.required]),
+      password: this.formBuilder.control('', [Validators.required]),
+    });
   }
 
 }
