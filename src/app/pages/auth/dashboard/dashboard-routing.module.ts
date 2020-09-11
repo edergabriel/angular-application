e5cdd0d6681fc60940293fc43ef037a316b7a78d
Eder Gabriel <eder1602@outlook.com>
2020-09-10 23:52:25 -0300

    Página de resumo

--- src/app/pages/auth/dashboard/dashboard-routing.module.ts
+++ src/app/pages/auth/dashboard/dashboard-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { SummaryComponent } from '../summary/summary.component';
 
 
 const routes: Routes = [
@@ -9,6 +10,7 @@ const routes: Routes = [
     path: '', 
     component: DashboardComponent,
     children: [ 
+      { path: '', component: SummaryComponent }, 
       { path: 'product-detail', component: ProductDetailComponent } 
     ]
   } 
--- src/app/pages/auth/dashboard/dashboard.module.ts
+++ src/app/pages/auth/dashboard/dashboard.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { SummaryComponent } from '../summary/summary.component';
 
 @NgModule({
-  declarations: [DashboardComponent, ProductDetailComponent],
+  declarations: [DashboardComponent, ProductDetailComponent, SummaryComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule
